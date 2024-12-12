29d8a6030a059bbba19b5de1c1debeccb4d61b29
evans <evandaniil85@gmail.com>
2024-12-12 22:08:34 +0300

    all fix

--- app/Services/Telegram/ModuleBot/FatherBotController.php
+++ app/Services/Telegram/ModuleBot/FatherBotController.php
@@ -26,7 +26,7 @@ class FatherBotController extends AbstractTelegramBot
     protected function processUpdate(): void
     {
         try {
-            if ($this->update->getMessage()->getText() === '/start') {
+            if ($this->update->getMessage()->text === '/start') {
                 $this->userState = null;
                 $this->start();
                 return;
