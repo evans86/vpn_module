4df5fed36e67de19c23f6ef86cee639fb1b895f5
evans <evandaniil85@gmail.com>
2024-12-14 16:12:39 +0300

    all fix

--- app/Services/Telegram/ModuleBot/FatherBotController.php
+++ app/Services/Telegram/ModuleBot/FatherBotController.php
@@ -37,7 +37,7 @@ class FatherBotController extends AbstractTelegramBot
             $message = $this->update->getMessage();
             $callbackQuery = $this->update->callbackQuery;
 
-            Log::debug('userState: ' . $this->userState);
+            Log::debug('UserState: ' . $this->userState);
 
             // Проверяем состояние ожидания токена
             if ($this->userState === self::STATE_WAITING_TOKEN && $message) {
