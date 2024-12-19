5c9ce4c3cde68bcb6bed44c2df05f6959b81aee5
evans <evandaniil85@gmail.com>
2024-12-19 23:36:02 +0300

    all fix

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -5,6 +5,7 @@ namespace App\Http;
 use App\Http\Middleware\AdminAccess;
 use App\Http\Middleware\Authenticate;
 use App\Http\Middleware\EncryptCookies;
+use App\Http\Middleware\ForceHttps;
 use App\Http\Middleware\PreventRequestsDuringMaintenance;
 use App\Http\Middleware\RedirectIfAuthenticated;
 use App\Http\Middleware\TrimStrings;
@@ -45,6 +46,7 @@ class Kernel extends HttpKernel
         ValidatePostSize::class,
         TrimStrings::class,
         ConvertEmptyStringsToNull::class,
+        ForceHttps::class,
     ];
 
     /**
