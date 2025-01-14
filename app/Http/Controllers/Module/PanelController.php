70e700c3e155cc9aca4186a84feab291ab0528b9
evans <evandaniil85@gmail.com>
2025-01-14 23:12:40 +0300

    add transfer user

--- app/Http/Controllers/Module/PanelController.php
+++ app/Http/Controllers/Module/PanelController.php
@@ -106,17 +106,17 @@ class PanelController extends Controller
     public function store(Request $request): RedirectResponse
     {
         try {
-            $validated = $request->validate([
-                'server_id' => ['required', 'exists:server,id'],
-                'panel_adress' => ['required', 'string', 'max:255'],
-                'panel_port' => ['required', 'integer', 'min:1', 'max:65535'],
-            ]);
+//            $validated = $request->validate([
+//                'server_id' => ['required', 'exists:server,id'],
+//                'panel_adress' => ['required', 'string', 'max:255'],
+//                'panel_port' => ['required', 'integer', 'min:1', 'max:65535'],
+//            ]);
 
             DB::beginTransaction();
 
             // Создаем панель
             $panel = Panel::create([
-                'server_id' => $validated['server_id'],
+                'server_id' => $request['server_id'],
                 'panel_adress' => $validated['panel_adress'],
                 'panel_port' => $validated['panel_port'],
                 'panel_status' => Panel::PANEL_CREATED
