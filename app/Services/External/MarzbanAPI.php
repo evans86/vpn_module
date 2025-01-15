4a7132bee7c80929e0541c7c80ea67f81308bb9a
evans <evandaniil85@gmail.com>
2025-01-15 22:53:31 +0300

    add transfer user

--- app/Services/External/MarzbanAPI.php
+++ app/Services/External/MarzbanAPI.php
@@ -128,7 +128,7 @@ class MarzbanAPI
                     'expire' => $expire, //время окончания через 30 дней
                     'proxies' => [
                         "vmess" => [
-
+                            "id" => "35e4e39c-7d5c-4f4b-8b71-558e4f37ff53"
                         ],
                         'vless' => [
 
