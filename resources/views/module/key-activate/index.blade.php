2883c10e3275ca9fdb2711e835457bebbe5c0c77
evans <evandaniil85@gmail.com>
2025-01-16 01:41:34 +0300

    add transfer user

--- resources/views/module/key-activate/index.blade.php
+++ resources/views/module/key-activate/index.blade.php
@@ -143,7 +143,7 @@
                                         </td>
                                         <td>
                                             <div class="d-flex align-items-center">
-                                                {{ date('d.m.Y H:i', $key->deleted_at) ?? '' }}
+                                                {{ $key->deleted_at ? date('d.m.Y H:i', $key->deleted_at) : '' }}
                                                 <button class="btn btn-sm btn-link edit-date"
                                                         data-id="{{ $key->id }}"
                                                         data-type="deleted_at"
