05fa403e9c206980f0e9916505ff6f05a137d14c
evans <evandaniil85@gmail.com>
2025-01-28 00:45:54 +0300

    reload bot

--- resources/views/module/pack-salesman/index.blade.php
+++ resources/views/module/pack-salesman/index.blade.php
@@ -114,7 +114,7 @@
                                         <td>{{ $pack_salesman->created_at->format('d.m.Y H:i') }}</td>
                                         <td>
                                             <div class="btn-group">
-                                                @if(!$pack_salesman->status === 1)
+                                                @if(!$pack_salesman->isPaid())
                                                     <button type="button"
                                                             class="btn btn-sm btn-success"
                                                             onclick="markAsPaid({{ $pack_salesman->id }})">
