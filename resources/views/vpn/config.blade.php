557c4bc3cf5f723659a8a211ee666672f5045ab9
evans <evandaniil85@gmail.com>
2025-02-08 18:22:01 +0300

    reload bot

--- resources/views/vpn/config.blade.php
+++ resources/views/vpn/config.blade.php
@@ -128,6 +128,7 @@
 
     @push('scripts')
         <script src="https://cdnjs.cloudflare.com/ajax/libs/qrcodejs/1.0.0/qrcode.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/qrcode-generator@1.4.4/qrcode.min.js"></script>
         <script>
             function copyCurrentUrl() {
                 const url = window.location.href;
