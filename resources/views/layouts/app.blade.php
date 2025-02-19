7fb6deb456b76b8bc3e94a2d9934a35327965f46
evans <evandaniil85@gmail.com>
2025-02-19 23:47:50 +0300

    clean all services

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -64,7 +64,7 @@
 
 <!-- Required Scripts -->
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/4.6.2/js/bootstrap.bundle.min.js"></script>
 <script
     src="{{ \App\Helpers\AssetHelper::asset('mota/centre/bootstrap-select/dist/js/bootstrap-select.min.js') }}"></script>
 <script src="{{ \App\Helpers\AssetHelper::asset('mota/centre/chartist/js/chartist.min.js') }}"></script>
