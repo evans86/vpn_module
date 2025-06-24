ba26067ce585e9b4ad3d371eba5d865d67745d42
evans <evandaniil85@gmail.com>
2025-06-24 21:02:35 +0300

    update support

--- resources/views/vpn/config.blade.php
+++ resources/views/vpn/config.blade.php
@@ -5,12 +5,12 @@
         <div class="bg-white rounded-lg shadow-lg p-6">
             <!-- Bot Link and Copy URL Section -->
             <div class="mb-8 flex justify-between items-center">
-                <div>
-                    <span class="text-gray-600">Бот активации:</span>
-                    <a href="{{ $botLink }}" target="_blank" class="ml-2 text-blue-600 hover:text-blue-800">
-                        Перейти к боту
-                    </a>
-                </div>
+{{--                <div>--}}
+{{--                    <span class="text-gray-600">Бот активации:</span>--}}
+{{--                    <a href="{{ $botLink }}" target="_blank" class="ml-2 text-blue-600 hover:text-blue-800">--}}
+{{--                        Перейти к боту--}}
+{{--                    </a>--}}
+{{--                </div>--}}
                 <button onclick="copyCurrentUrl()"
                         class="inline-flex items-center px-3 py-2 border border-gray-300 shadow-sm text-sm leading-4 font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 transition-colors">
                     <svg class="h-4 w-4 mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24">
