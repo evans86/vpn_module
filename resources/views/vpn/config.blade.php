87c64aa56781e13eb6eeae99cb59815405314a93
evans <evandaniil85@gmail.com>
2025-09-06 09:36:34 +0300

    fix panel

--- resources/views/vpn/config.blade.php
+++ resources/views/vpn/config.blade.php
@@ -11,7 +11,7 @@
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                               d="M8 5H6a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2v-1M8 5a2 2 0 002 2h2a2 2 0 002-2M8 5a2 2 0 012-2h2a2 2 0 012 2m0 0h2a2 2 0 012 2v3m2 4H10m0 0l3-3m-3 3l3 3"/>
                     </svg>
-                    Скопировать ссылку
+                    Скопировать ссылку конфигурации
                 </button>
 
                 <!-- Новая кнопка для QR общей ссылки -->
@@ -20,7 +20,7 @@
                     <svg class="h-4 w-4 mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 9h14a2 2 0 012 2v2m0 0H3a2 2 0 01-2-2V9a2 2 0 012-2h14a2 2 0 012 2v2zm0 0h2a2 2 0 012 2v4a2 2 0 01-2 2H5a2 2 0 01-2-2v-4a2 2 0 012-2h2z"/>
                     </svg>
-                    QR общей ссылки
+                    QR-код конфигурации
                 </button>
             </div>
 
