a9e3a511f29215f15aab3ca7f9e9acddd9384d93
evans <evandaniil85@gmail.com>
2025-11-12 20:52:43 +0300

    add monitoring

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,7 +14,6 @@ class Kernel extends ConsoleKernel
      */
     protected $commands = [
         \App\Console\Commands\MonitorConnectionLimits::class,
-        \App\Console\Commands\DebugLogs::class,
     ];
 
     /**
